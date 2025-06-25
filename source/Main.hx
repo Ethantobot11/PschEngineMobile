2dd0f95d39afc6831cd64c56dd7cb98263c729e9
Ethantobot11 <156458071+Ethantobot11@users.noreply.github.com>
2025-06-25 21:03:57 +0200

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -1,88 +1,134 @@
 package;
 
-#if android
-import android.content.Context;
-#end
-
-import debug.FPSCounter;
-
+import online.GameClient;
+import states.MainMenuState;
+import externs.WinAPI;
+import haxe.Exception;
 import flixel.graphics.FlxGraphic;
+import haxe.io.Path;
 import flixel.FlxGame;
 import flixel.FlxState;
-import haxe.io.Path;
 import openfl.Assets;
 import openfl.Lib;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.app.Application;
 import states.TitleState;
-
-#if HSCRIPT_ALLOWED
-import crowplexus.iris.Iris;
-import psychlua.HScript.HScriptInfos;
+#if mobile
+import mobile.backend.MobileScaleMode;
 #end
 
 #if (linux || mac)
 import lime.graphics.Image;
 #end
 
-#if desktop
-import backend.ALSoftConfig; // Just to make sure DCE doesn't remove this, since it's not directly referenced anywhere else.
-#end
+import backend.io.PsychFileSystem as FileSystem;
 
-//crash handler stuff
-#if CRASH_HANDLER
-import openfl.events.UncaughtErrorEvent;
-import haxe.CallStack;
-import haxe.io.Path;
-#end
-
-import backend.Highscore;
-
-// NATIVE API STUFF, YOU CAN IGNORE THIS AND SCROLL //
 #if (linux && !debug)
 @:cppInclude('./external/gamemode_client.h')
 @:cppFileCode('#define GAMEMODE_AUTO')
 #end
-
-// // // // // // // // //
 class Main extends Sprite
 {
-	public static final game = {
+	public static var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
 		initialState: TitleState, // initial game state
+		zoom: -1.0, // game state bounds
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
 		startFullscreen: false // if the game should start at fullscreen mode
 	};
 
-	public static var fpsVar:FPSCounter;
+	public static var fpsVar:FPS;
+
+	public static final PSYCH_ONLINE_VERSION:String = "0.11.9";
+	public static final CLIENT_PROTOCOL:Float = 8;
+	public static final GIT_COMMIT:String = online.backend.Macros.getGitCommitHash();
+	public static final LOW_STORAGE:Bool = online.backend.Macros.hasNoCapacity();
+	public static var UNOFFICIAL_BUILD:Bool = Main.LOW_STORAGE;
+
+	public static var wankyUpdate:String = 'version';
+	public static var latestRelease:Dynamic = {};
+	public static var updateVersion:String = '';
+
+	public static var view3D:online.away.View3DHandler;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
+		#if !mobile // would crash the game
+		if (Path.normalize(Sys.getCwd()) != Path.normalize(lime.system.System.applicationDirectory)) {
+			Lib.application.window.alert("Your path is either not run from the game directory,\nor contains illegal UTF-8 characters!\n\nRun from: "
+				+ Sys.getCwd()
+				+ "\nExpected path: " + lime.system.System.applicationDirectory, 
+			"Invalid Runtime Path!");
+			Sys.exit(1);
+		}
+		#end
+		
+		Lib.current.addChild(view3D = new online.away.View3DHandler());
 		Lib.current.addChild(new Main());
+		Lib.current.addChild(new online.gui.sidebar.SideUI());
+		Lib.current.addChild(new online.gui.Alert());
+		Lib.current.addChild(new online.gui.LoadingScreen());
 	}
 
 	public function new()
 	{
 		super();
+		#if mobile
+		#if android
+		StorageUtil.requestPermissions();
+		#end
+		Sys.setCwd(StorageUtil.getStorageDirectory());
+		#end
+		backend.CrashHandler.init();
 
 		#if (cpp && windows)
 		backend.Native.fixScaling();
 		#end
 
-		// Credits to MAJigsaw77 (he's the og author for this code)
-		#if android
-		Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
-		#elseif ios
-		Sys.setCwd(lime.system.System.applicationStorageDirectory);
-		#end
-		#if VIDEOS_ALLOWED
-		hxvlc.util.Handle.init(#if (hxvlc >= "1.8.0")  ['--no-lua'] #end);
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+	}
+
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+
+	private function setupGame():Void
+	{
+		#if (openfl <= "9.2.0")
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (game.zoom == -1.0)
+		{
+			var ratioX:Float = stageWidth / game.width;
+			var ratioY:Float = stageHeight / game.height;
+			game.zoom = Math.min(ratioX, ratioY);
+			game.width = Math.ceil(stageWidth / game.zoom);
+			game.height = Math.ceil(stageHeight / game.zoom);
+		}
+		#else
+		if (game.zoom == -1.0)
+			game.zoom = 1.0;
 		#end
 
 		#if LUA_ALLOWED
@@ -90,84 +136,43 @@ class Main extends Sprite
 		#end
 		Mods.loadTopMod();
 
-		FlxG.save.bind('funkin', CoolUtil.getSavePath());
-		Highscore.load();
+		CoolUtil.setDarkMode(true);
 
-		#if HSCRIPT_ALLOWED
-		Iris.warn = function(x, ?pos:haxe.PosInfos) {
-			Iris.logLevel(WARN, x, pos);
-			var newPos:HScriptInfos = cast pos;
-			if (newPos.showLine == null) newPos.showLine = true;
-			var msgInfo:String = (newPos.funcName != null ? '(${newPos.funcName}) - ' : '')  + '${newPos.fileName}:';
-			#if LUA_ALLOWED
-			if (newPos.isLua == true) {
-				msgInfo += 'HScript:';
-				newPos.showLine = false;
-			}
-			#end
-			if (newPos.showLine == true) {
-				msgInfo += '${newPos.lineNumber}:';
-			}
-			msgInfo += ' $x';
-			if (PlayState.instance != null)
-				PlayState.instance.addTextToDebug('WARNING: $msgInfo', FlxColor.YELLOW);
+		#if lumod
+		Lumod.addons.push(online.backend.LuaModuleSwap.LumodModuleAddon);
+		Lumod.scriptPathHandler = scriptPath -> {
+			var defaultPath:String = 'lumod/' + scriptPath;
+
+			// check if script exists in any of loaded mods
+			var path:String = Paths.modFolders(defaultPath);
+			if (FileSystem.exists(path))
+				return path;
+
+			return defaultPath;
 		}
-		Iris.error = function(x, ?pos:haxe.PosInfos) {
-			Iris.logLevel(ERROR, x, pos);
-			var newPos:HScriptInfos = cast pos;
-			if (newPos.showLine == null) newPos.showLine = true;
-			var msgInfo:String = (newPos.funcName != null ? '(${newPos.funcName}) - ' : '')  + '${newPos.fileName}:';
-			#if LUA_ALLOWED
-			if (newPos.isLua == true) {
-				msgInfo += 'HScript:';
-				newPos.showLine = false;
-			}
-			#end
-			if (newPos.showLine == true) {
-				msgInfo += '${newPos.lineNumber}:';
-			}
-			msgInfo += ' $x';
-			if (PlayState.instance != null)
-				PlayState.instance.addTextToDebug('ERROR: $msgInfo', FlxColor.RED);
-		}
-		Iris.fatal = function(x, ?pos:haxe.PosInfos) {
-			Iris.logLevel(FATAL, x, pos);
-			var newPos:HScriptInfos = cast pos;
-			if (newPos.showLine == null) newPos.showLine = true;
-			var msgInfo:String = (newPos.funcName != null ? '(${newPos.funcName}) - ' : '')  + '${newPos.fileName}:';
-			#if LUA_ALLOWED
-			if (newPos.isLua == true) {
-				msgInfo += 'HScript:';
-				newPos.showLine = false;
-			}
-			#end
-			if (newPos.showLine == true) {
-				msgInfo += '${newPos.lineNumber}:';
-			}
-			msgInfo += ' $x';
-			if (PlayState.instance != null)
-				PlayState.instance.addTextToDebug('FATAL: $msgInfo', 0xFFBB0000);
-		}
+		Lumod.classResolver = Deflection.resolveClass;
+		Lumod.initializeLuaCallbacks = false;
 		#end
 
+		#if hl
+		sys.ssl.Socket.DEFAULT_VERIFY_CERT = false;
+		#end
+	
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
-		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
-		addChild(new FlxGame(game.width, game.height, game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
-		#if !mobile
-		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
+		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.data.showFPS;
 		}
-		#end
 
-		#if (linux || mac) // fix the app icon not showing up on the Linux Panel / Mac Dock
-		var icon = Image.fromFile("icon.png");
+		#if (linux || mac)
+		final icon:Image = Image.fromFile("icon.png");
 		Lib.current.stage.window.setIcon(icon);
 		#end
 
@@ -177,29 +182,132 @@ class Main extends Sprite
 		#end
 
 		FlxG.fixedTimestep = false;
-		FlxG.game.focusLostFramerate = 60;
+		FlxG.game.focusLostFramerate = #if mobile 30 #else 60 #end;
+		#if web
+		FlxG.keys.preventDefaultKeys.push(TAB);
+		#else
 		FlxG.keys.preventDefaultKeys = [TAB];
-		
-		#if CRASH_HANDLER
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 		#end
 
+		#if android FlxG.android.preventDefaultKeys = [BACK]; #end
+
 		#if DISCORD_ALLOWED
-		DiscordClient.prepare();
+		DiscordClient.start();
 		#end
+
+		#if mobile
+		lime.system.System.allowScreenTimeout = ClientPrefs.data.screensaver; 		
+		FlxG.scaleMode = new MobileScaleMode();
+		#end
+
+		Application.current.window.vsync = ClientPrefs.data.vsync;
 
 		// shader coords fix
 		FlxG.signals.gameResized.add(function (w, h) {
+			if(fpsVar != null)
+				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
+
 		     if (FlxG.cameras != null) {
 			   for (cam in FlxG.cameras.list) {
+				@:privateAccess
 				if (cam != null && cam.filters != null)
 					resetSpriteCache(cam.flashSprite);
 			   }
-			}
+		     }
 
-			if (FlxG.game != null)
-			resetSpriteCache(FlxG.game);
+		     if (FlxG.game != null)
+			 resetSpriteCache(FlxG.game);
 		});
+
+		//ONLINE STUFF, BELOW CODE USE FOR BACKPORTING
+
+		var http = new haxe.Http("https://raw.githubusercontent.com/Snirozu/Funkin-Psych-Online/main/server_addresses.txt");
+		http.onData = function(data:String) {
+			for (address in data.split(',')) {
+				online.GameClient.serverAddresses.push(address.trim());
+			}
+		}
+		http.onError = function(error) {
+			trace('error: $error');
+		}
+		http.request();
+		#if LOCAL
+		online.GameClient.serverAddresses.insert(0, "ws://localhost:2567");
+		#else
+		online.GameClient.serverAddresses.push("ws://localhost:2567");
+		#end
+		online.network.FunkinNetwork.client = new online.http.HTTPHandler(online.GameClient.addressToUrl());
+
+		online.mods.ModDownloader.checkDeleteDlDir();
+
+		addChild(new online.gui.DownloadAlert.DownloadAlerts());
+
+		FlxG.plugins.add(new online.backend.Waiter());
+
+		online.backend.Thread.repeat(() -> {
+			try {
+				online.network.FunkinNetwork.ping();
+			}
+			catch (exc) {
+				trace(exc);
+			}
+		}, 60, _ -> {}); // ping the server every minute
+		
+		//for some reason only cancels 2 downloads
+		Lib.application.window.onClose.add(() -> {
+			#if DISCORD_ALLOWED
+			DiscordClient.shutdown();
+			#end
+			online.mods.ModDownloader.cancelAll();
+			online.mods.ModDownloader.checkDeleteDlDir();
+			online.network.Auth.saveClose();
+		});
+
+		#if !mobile
+		Lib.application.window.onDropFile.add(path -> {
+			if (FileSystem.isDirectory(path))
+				return;
+
+			if (path.endsWith(".json") && (path.contains("-chart") || path.contains("-metadata"))) {
+				online.util.vslice.VUtil.convertVSlice(path);
+			}
+			else {
+				online.backend.Thread.run(() -> {
+					online.gui.LoadingScreen.toggle(true);
+					online.mods.OnlineMods.installMod(path);
+					online.gui.LoadingScreen.toggle(false);
+				});
+			}
+		});
+		#end
+
+		// clear messages before the current state gets destroyed and replaced with another
+		FlxG.signals.preStateSwitch.add(() -> {
+			GameClient.clearOnMessage();
+		});
+
+		FlxG.signals.postGameReset.add(() -> {
+			online.gui.Alert.alert('Warning!', 'The game has been resetted, and there may occur visual bugs with the sidebar!\n\nIt\'s recommended to restart the game instead.');
+		});
+		
+		#if HSCRIPT_ALLOWED
+		FlxG.signals.postStateSwitch.add(() -> {
+			online.backend.SyncScript.dispatch("switchState", [FlxG.state]);
+
+			FlxG.state.subStateOpened.add(substate -> {
+				online.backend.SyncScript.dispatch("openSubState", [substate]);
+			});
+		});
+
+		FlxG.signals.postUpdate.add(() -> {
+			if (online.backend.SyncScript.activeUpdate)
+				online.backend.SyncScript.dispatch("update", [FlxG.elapsed]);
+		});
+
+		online.backend.SyncScript.resyncScript(false, () -> {
+			online.backend.SyncScript.dispatch("init");
+		});
+		#end
 	}
 
 	static function resetSpriteCache(sprite:Sprite):Void {
@@ -209,53 +317,20 @@ class Main extends Sprite
 		}
 	}
 
-	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
-	// very cool person for real they don't get enough credit for their work
-	#if CRASH_HANDLER
-	function onCrash(e:UncaughtErrorEvent):Void
-	{
-		var errMsg:String = "";
-		var path:String;
-		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
-		var dateNow:String = Date.now().toString();
-
-		dateNow = dateNow.replace(" ", "_");
-		dateNow = dateNow.replace(":", "'");
-
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
-
-		for (stackItem in callStack)
-		{
-			switch (stackItem)
-			{
-				case FilePos(s, file, line, column):
-					errMsg += file + " (line " + line + ")\n";
-				default:
-					Sys.println(stackItem);
-			}
-		}
-
-		errMsg += "\nUncaught Error: " + e.error;
-		// remove if you're modding and want the crash log message to contain the link
-		// please remember to actually modify the link for the github page to report the issues to.
-		#if officialBuild
-		errMsg += "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine";
+	public static function getTime():Float {
+		#if flash
+		return flash.Lib.getTimer();
+		#elseif ((js && !nodejs) || electron)
+		return js.Browser.window.performance.now();
+		#elseif sys
+		return Sys.time() * 1000;
+		#elseif (lime_cffi && !macro)
+		@:privateAccess
+		return cast lime._internal.backend.native.NativeCFFI.lime_system_get_timer();
+		#elseif cpp
+		return untyped __global__.__time_stamp() * 1000;
+		#else
+		return 0;
 		#end
-		errMsg += "\n\n> Crash Handler written by: sqirra-rng";
-
-		if (!FileSystem.exists("./crash/"))
-			FileSystem.createDirectory("./crash/");
-
-		File.saveContent(path, errMsg + "\n");
-
-		Sys.println(errMsg);
-		Sys.println("Crash dump saved in " + Path.normalize(path));
-
-		Application.current.window.alert(errMsg, "Error!");
-		#if DISCORD_ALLOWED
-		DiscordClient.shutdown();
-		#end
-		Sys.exit(1);
 	}
-	#end
 }
--- source/import.hx
+++ source/import.hx
@@ -10,15 +10,21 @@ import llua.*;
 import llua.Lua;
 #end
 
-#if ACHIEVEMENTS_ALLOWED
-import backend.Achievements;
+#if flxanimate
+import flxanimate.FlxAnimate;
 #end
 
-#if sys
-import sys.*;
-import sys.io.*;
-#elseif js
-import js.html.*;
+// Mobile Controls
+import mobile.objects.Hitbox;
+import mobile.objects.TouchPad;
+import mobile.objects.TouchButton;
+import mobile.input.MobileInputID;
+import mobile.backend.MobileData;
+import mobile.input.MobileInputManager;
+import mobile.backend.TouchUtil;
+
+#if lumod
+import lumod.Lumod;
 #end
 
 import backend.Paths;
@@ -32,9 +38,20 @@ import backend.Conductor;
 import backend.BaseStage;
 import backend.Difficulty;
 import backend.Mods;
-import backend.Language;
+import mobile.backend.StorageUtil;
 
-import backend.ui.*; //Psych-UI
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
 
 import objects.Alphabet;
 import objects.BGSprite;
@@ -42,18 +59,17 @@ import objects.BGSprite;
 import states.PlayState;
 import states.LoadingState;
 
-#if flxanimate
-import flxanimate.*;
-import flxanimate.PsychFlxAnimate as FlxAnimate;
-#end
-
 //Flixel
+#if (flixel >= "5.3.0")
 import flixel.sound.FlxSound;
+#else
+import flixel.system.FlxSound;
+#end
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
+import flixel.util.FlxDestroyUtil;
 import flixel.math.FlxMath;
-import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import flixel.text.FlxText;
@@ -61,7 +77,8 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.addons.transition.FlxTransitionableState;
+import shaders.flixel.system.FlxShader;
+import online.backend.Deflection;
 
 using StringTools;
 #end
